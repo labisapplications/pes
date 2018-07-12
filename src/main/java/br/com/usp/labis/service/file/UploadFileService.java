2b924fe7de667347407b634fce9fd14d23950f2c
tgmangolini <taniagmangolini@gmail.com>
2018-07-12 07:28:50 -0300

    test

--- src/main/java/br/com/usp/labis/service/file/UploadFileService.java
+++ src/main/java/br/com/usp/labis/service/file/UploadFileService.java
@@ -35,42 +35,27 @@ public class UploadFileService {
 			// Get the file and save it in the UPLOADED_FOLDER
 			byte[] bytes = file.getBytes();
 			
-			System.out.println("OPENSHIFT_DATA_DIR: " + System.getenv("OPENSHIFT_DATA_DIR"));
-			System.out.println("OPENSHIFT_REPO_DIR: " + System.getenv("OPENSHIFT_REPO_DIR"));
-
-			
-			Path teste = FileSystems.getDefault().getPath("/api/v1/namespaces/pes/persistentvolumeclaims/upload", newFileName);
-			System.out.println( "teste1" + teste);
+			Path teste2 = FileSystems.getDefault().getPath(newFileName);
+			System.out.println( "teste2" + teste2);
 			try {
-				Files.write(teste, bytes);
+				Files.write(teste2, bytes);
 			} catch (Exception e) {
 				System.out.println("teste1 erro: " + e.getMessage() + e.getCause());
 				e.printStackTrace();
 			}
 			
-			Path teste2 = FileSystems.getDefault().getPath("/upload ", newFileName);
-			System.out.println( "teste2" + teste);
+						
+			Path teste1 = FileSystems.getDefault().getPath("/upload", newFileName);
+			System.out.println( "teste1" + teste1);
 			try {
-				Files.write(teste2, bytes);
+				Files.write(teste1, bytes);
 			} catch (Exception e) {
-				System.out.println("teste2 erro: " + e.getMessage() + e.getCause());
+				System.out.println("teste1 erro: " + e.getMessage() + e.getCause());
 				e.printStackTrace();
-			}
-
-			Path teste3 = FileSystems.getDefault().getPath("/data ", newFileName);
-			System.out.println( "teste3" + teste);
-			try {
-				Files.write(teste3, bytes);
-			} catch (Exception e) {
-				System.out.println("teste3 erro: " + e.getMessage() + e.getCause());
-				e.printStackTrace();
-			}
-
-
-			//Path path = Paths.get(UPLOADED_FOLDER + newFileName);
-			System.out.println( "SOPENSHIFT_DATA_DIR" + System.getenv("OPENSHIFT_DATA_DIR") );
-			Path path = FileSystems.getDefault().getPath(System.getenv("OPENSHIFT_DATA_DIR"), newFileName);
-			System.out.println( "path" + path );
+			} 
+			
+			
+			Path path = FileSystems.getDefault().getPath(UPLOADED_FOLDER, newFileName);
 			Files.write(path, bytes);
 			uploadedFile = new File(UPLOADED_FOLDER + newFileName);
 			
